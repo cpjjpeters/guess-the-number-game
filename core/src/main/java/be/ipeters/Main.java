861a1b7f46383db6cbd7590f024a7d8cad7a4051
cpjjpeters <cpjjpeters@gmail.com>
2019-06-28 09:40:45 +0200

    remove beans.xml add AppConfig.class

--- core/src/main/java/be/ipeters/Main.java
+++ core/src/main/java/be/ipeters/Main.java
@@ -3,17 +3,16 @@ package be.ipeters;
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     private final static Logger log = (Logger) LoggerFactory.getLogger(Main.class);
-    private  static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
-        log.info("Guess the number Game");
+        log.info("Guess the number Game without beans.xml");
 
         // create context (container)
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         NumberGenerator numberGenerator = context.getBean( NumberGenerator.class);
 
         // call method next()
