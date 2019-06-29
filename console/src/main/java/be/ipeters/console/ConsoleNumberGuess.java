2fbaf149cd2910c928050fdb7fc759b7f0166973
cpjjpeters <cpjjpeters@gmail.com>
2019-06-29 15:09:24 +0200

    removed implements ApplicationListener<ContextRefreshedEvent> and then using @EventListener

--- console/src/main/java/be/ipeters/console/ConsoleNumberGuess.java
+++ console/src/main/java/be/ipeters/console/ConsoleNumberGuess.java
@@ -1,17 +1,17 @@
 package be.ipeters.console;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        log.info("Container ready for use.");
+    @EventListener
+    public void startApplicationEvent(ContextRefreshedEvent event) {
+        log.info("START: Container ready for use.");
     }
     // == constants
 //    private static final Logger log = (Logger) LoggerFactory.getLogger(ConsoleNumberGuess.class);
